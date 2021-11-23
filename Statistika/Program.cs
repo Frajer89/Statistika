0edadc9e031bfbaf4df1b23aec4682059d7b583f
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2021-11-23 09:40:51 +0100

    hotovo

--- Statistika/Program.cs
+++ Statistika/Program.cs
@@ -7,11 +7,44 @@ namespace Statistika
         static void Main(string[] args)
         {
             ConsoleKeyInfo keyInfo;
-
+            string numbers = "0123456789";
+            string text = "";
+            int rowNumber = 0;
             do
             {
-                keyInfo = Console.ReadKey(false);
+                keyInfo = Console.ReadKey(true);       //Zachycení stisknuté
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    //když uživatel zmáčkne Enter
+                    Console.WriteLine();
+                    rowNumber++;
+                    text = "";
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace && text.Length > 0)
+                {
+                    //když uživatel zmáčkne Backspace
+                    //přepíšu řádek mezerama - počet podle délky textu
+                    Console.SetCursorPosition(0, rowNumber);
+                    Console.Write(new string(' ', text.Length));
+                    
+
+                    //zkrátím text
+                    text = text.Substring(0, text.Length - 1);
+
+                    //napíšu zkrácený text
+                    Console.SetCursorPosition(0, rowNumber);
+                    Console.Write(text);
+                }
+                else if (numbers.Contains(keyInfo.KeyChar))
+                {
+                    //když uživatel napsal čislici
+                    text += keyInfo.KeyChar;
+                    Console.SetCursorPosition(0, rowNumber);
+                    Console.Write(text);
+                }
             } while (keyInfo.Key != ConsoleKey.Escape);
+            
+                
         }  
     }
 }
